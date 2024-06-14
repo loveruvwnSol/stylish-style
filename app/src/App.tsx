a4e69562f9dfe0c1ef49b1b2d86179f26323fe86
sol <ocosio0907@gmail.com>
2024-06-14 11:07:28 +0900

    add session control

---
+++ app/src/App.tsx
@@ -0,0 +1,33 @@
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { CreateAccount } from "./pages/CreateAccount";
+import { Home } from "./pages/Home";
+import { useEffect, useState } from "react";
+import { supabase } from "./libs/supabaseClient";
+import { Session } from "@supabase/supabase-js";
+
+const App = () => {
+  const navigate = useNavigate();
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    const session = async () => {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+      if (session == null || document.referrer === "") navigate("/login");
+      else console.log(session);
+      setSession(session);
+    };
+    session();
+  });
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/createAccount" element={<CreateAccount />} />
+    </Routes>
+  );
+};
+
+export default App;
