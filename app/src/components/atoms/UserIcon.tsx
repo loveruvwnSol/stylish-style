999b92c9ce4bb54dabb94b47915e234f2f7b0957
sol <ocosio0907@gmail.com>
2024-06-16 00:13:46 +0900

    add useUserIcon

--- app/src/components/atoms/UserIcon.tsx
+++ app/src/components/atoms/UserIcon.tsx
@@ -1,14 +1,20 @@
-import { Box } from '@chakra-ui/react';
+import { AspectRatio, Box, Image } from "@chakra-ui/react";
+import { useUserIcon } from "../../hooks/useUser";
 
 const UserIcon = () => {
+  const userIcon = useUserIcon();
   return (
     <Box
-      border={'solid 1px'}
-      borderRadius={'50%'}
-      w={90}
-      h={90}
-      m={'0 auto'}
-    ></Box>
+      border={"solid 1px lightgray"}
+      borderRadius={"50%"}
+      w={100}
+      h={100}
+      m={"0 auto"}
+    >
+      <AspectRatio maxW="500px" ratio={1}>
+        <Image borderRadius={"50%"} src={userIcon} />
+      </AspectRatio>
+    </Box>
   );
 };
 
