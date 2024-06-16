ef5b3c5ad0f331593f657513facfce211ad7d3d0
sol <ocosio0907@gmail.com>
2024-06-16 15:58:06 +0900

    refactoring

--- app/src/hooks/useUser.ts
+++ app/src/hooks/useUser.ts
@@ -4,19 +4,20 @@ import { supabase } from "../libs/supabaseClient";
 export const useUser = () => {
   const [user, setUser] = useState<any>();
   useEffect(() => {
-    const getCurrentUser = async () => {
-      const { data: currentUser } = await supabase.auth.getUser();
-
-      const { data, error } = await supabase
-        .from("users")
-        .select()
-        .eq("id", currentUser.user?.id);
-      if (!error) {
-        setUser(data[0]);
-      }
-    };
     getCurrentUser();
   }, []);
+
+  const getCurrentUser = async () => {
+    const { data: currentUser } = await supabase.auth.getUser();
+
+    const { data, error } = await supabase
+      .from("users")
+      .select()
+      .eq("id", currentUser.user?.id);
+    if (!error) {
+      setUser(data[0]);
+    } else alert(error.message);
+  };
 
   return user;
 };
@@ -24,28 +25,28 @@ export const useUser = () => {
 export const useUserIcon = () => {
   const [userIcon, setUserIcon] = useState<any>();
   useEffect(() => {
-    const getUserIcon = async () => {
-      const { data: currentUser } = await supabase.auth.getUser();
-      const { data: icon } = await supabase.storage
-        .from("user_icons")
-        .getPublicUrl(currentUser.user?.id + "/icon");
-      console.log(icon.publicUrl);
-      if (
-        icon.publicUrl ===
-        "https://vgckzlkbdgwchjktpyij.supabase.co/storage/v1/object/public/user_icons/" +
-          currentUser.user?.id +
-          "/icon"
-      ) {
-        setUserIcon(icon.publicUrl);
-      } else {
-        const { data: default_icon } = await supabase.storage
-          .from("user_icons")
-          .getPublicUrl("default_icon");
-        setUserIcon(default_icon.publicUrl);
-      }
-    };
     getUserIcon();
   }, []);
+
+  const getUserIcon = async () => {
+    const { data: currentUser } = await supabase.auth.getUser();
+    const { data: icon } = await supabase.storage
+      .from("user_icons")
+      .getPublicUrl(currentUser.user?.id + "/icon");
+    if (
+      icon.publicUrl ===
+      "https://vgckzlkbdgwchjktpyij.supabase.co/storage/v1/object/public/user_icons/" +
+        currentUser.user?.id +
+        "/icon"
+    ) {
+      setUserIcon(icon.publicUrl);
+    } else {
+      const { data: default_icon } = await supabase.storage
+        .from("user_icons")
+        .getPublicUrl("default_icon");
+      setUserIcon(default_icon.publicUrl);
+    }
+  };
 
   return userIcon;
 };
