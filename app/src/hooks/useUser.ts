707145406aec5e7851f56880ea48ba659a483f1b
sol <ocosio0907@gmail.com>
2024-06-16 21:30:17 +0900

    fix getUserIcon

--- app/src/hooks/useUser.ts
+++ app/src/hooks/useUser.ts
@@ -37,26 +37,22 @@ export const useUserIcon = () => {
   const [userIcon, setUserIcon] = useState<any>();
   useEffect(() => {
     getUserIcon();
-  }, []);
+  }, [userIcon]);
 
   const getUserIcon = async () => {
     const { data: currentUser } = await supabase.auth.getUser();
     const { data: icon } = await supabase.storage
       .from("user_icons")
       .getPublicUrl(currentUser.user?.id + "/icon");
-    if (
-      icon.publicUrl ===
-      "https://vgckzlkbdgwchjktpyij.supabase.co/storage/v1/object/public/user_icons/" +
-        currentUser.user?.id +
-        "/icon"
-    ) {
-      setUserIcon(icon.publicUrl);
-    } else {
-      const { data: default_icon } = await supabase.storage
-        .from("user_icons")
-        .getPublicUrl("default_icon");
-      setUserIcon(default_icon.publicUrl);
-    }
+    setUserIcon(icon.publicUrl);
+    if (!userIcon) getDefaultIcon();
+  };
+
+  const getDefaultIcon = async () => {
+    const { data: default_icon } = await supabase.storage
+      .from("user_icons")
+      .getPublicUrl("default_icon");
+    setUserIcon(default_icon.publicUrl);
   };
 
   const uploadUserIcon = async (id: UUID, file: File) => {
